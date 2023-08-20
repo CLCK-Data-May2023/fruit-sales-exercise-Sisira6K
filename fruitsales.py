3145b845f262b1c5baa5c83580d5ba4dd6a7c1d0
sisira <sisira3128@gmail.com>
2023-08-20 12:48:41 -0400

    fruitsales and fruit.csv added

--- fruitsales.py
+++ fruitsales.py
@@ -1,1 +1,20 @@
-# add your code here
+"""In this exercise we will create a Pandas Dataframe with fruit sales information and 
+write the data to a CSV file. This exercise is based on the Lesson 1 exercise in the 
+Kaggle Learn Pandas course.
+Write a Python program that creates a Pandas Dataframe that matches the table below:
+
+Apples	Bananas
+2017 Sales	35	21
+2018 Sales	41	34
+Write the data to a file called fruit.csv in the project directory."""
+import pandas as pd
+columns = {"Apples":[35,41],"Bananas":[21,34]}
+index = ['2017 Sales','2018 Sales']
+df = pd.DataFrame(columns, index)
+df.to_csv("fruit.csv")
+print(df)
+
+
+
+
+
